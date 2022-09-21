15a1396b50ce52a299c917c6ae81ae143f8d42e4
imattking <matt@imattking.com>
2022-09-21 14:30:11 -0400

    update feature branch hierarchy

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,54 @@
+// Define HTML element variables
+/******************************* */
+const videoFrame = document.querySelector('#frame');
+const scan = document.querySelector('#start');
+const scanResult = document.querySelector('#result');
+const manualEntry = document.querySelector('#enter');
 
-document.querySelector('#enter').addEventListener('click', getFetch);
+
+// Start scan to parse upc from barcode
+/************************************** */
+scan.addEventListener('click', () => {
+    // toggle visibily to bring video preview into view
+    videoFrame.classList.toggle('hidden');
+
+    // initialize async/await
+    Quagga.init({
+        inputStream : {
+          name : "Live",
+          type : "LiveStream",
+          target: document.querySelector('#frame')  // highlight where in DOM to load preview
+        },
+        decoder : {
+          readers : ["upc_reader"] // specify which type of barcode reader
+        },
+      }, function(err) {
+          if (err) {
+              console.log(err);
+              return
+          }
+          console.log("Initialization finished. Ready to start");
+
+          // Start scanning the bardcode
+          Quagga.start() 
+          //Quagga.onProcessed(callback);
+
+          // On detection process results of scan
+          Quagga.onDetected(function(result) {
+            if(result.codeResult) {
+                console.log("result", result.codeResult.code); // log to console as test
+                //scanResult.innerText = `Result: ${result.codeResult.code}`; // place in DOM as preview
+                Quagga.stop();
+                videoFrame.classList.add('hidden'); // hide preview on completion
+            } else {
+                document.querySelector('#result').innerText = "not detected"; // display error text
+                videoFrame.classList.add('hidden'); // hide preivew on error
+            }
+        });
+      })
+    });
+
+
 
 // Fetch function for querying Food API to get results
 /***************************************************** */
